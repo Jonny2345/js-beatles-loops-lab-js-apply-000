e4f92ed2493e167668caaee52a352db00944a569
friendly-network-5255 <jonathanmura05@gmail.com>
2020-08-30 15:01:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout",
+"He hated the sound of his own voice"];
+
+//alters the array when called
+function johnLennonFacts(facts) {
   var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+    while (i < facts.length) {
+       facts[i] += "!!!";
+     i++;
+    }
+    console.log(facts);
   }
-  return array;
-}
+  
+ //calls the function to alter the array
+ johnLennonFacts(facts)
 
 function iLoveTheBeatles(number) {
   var array = [];
